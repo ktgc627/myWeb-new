f5e53508df4a5d138581ae4bc872165c6a14f496
Gopichand <ktgopichand627@gmail.com>
2025-05-26 19:39:40 +0530

    adding required field to input

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,26 +1,40 @@
-import createField from './form-fields.js';
+import createField from "./form-fields.js";
 
 async function createForm(formHref, submitHref) {
   const { pathname } = new URL(formHref);
   const resp = await fetch(pathname);
   const json = await resp.json();
 
-  const form = document.createElement('form');
+  const form = document.createElement("form");
   form.dataset.action = submitHref;
 
-  const fields = await Promise.all(json.data.map((fd) => createField(fd, form)));
+  const fields = await Promise.all(
+    json.data.map((fd) => createField(fd, form))
+  );
   fields.forEach((field) => {
     if (field) {
+      // Add required to inputs, selects, textareas
+      field.querySelectorAll?.("input, select, textarea").forEach((el) => {
+        if (
+          el.type !== "submit" &&
+          el.type !== "button" &&
+          el.type !== "hidden"
+        ) {
+          el.required = true;
+        }
+      });
       form.append(field);
     }
   });
 
   // group fields into fieldsets
-  const fieldsets = form.querySelectorAll('fieldset');
+  const fieldsets = form.querySelectorAll("fieldset");
   fieldsets.forEach((fieldset) => {
-    form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
-      fieldset.append(field);
-    });
+    form
+      .querySelectorAll(`[data-fieldset="${fieldset.name}"`)
+      .forEach((field) => {
+        fieldset.append(field);
+      });
   });
 
   return form;
@@ -30,11 +44,14 @@ function generatePayload(form) {
   const payload = {};
 
   [...form.elements].forEach((field) => {
-    if (field.name && field.type !== 'submit' && !field.disabled) {
-      if (field.type === 'radio') {
+    if (field.name && field.type !== "submit" && !field.disabled) {
+      if (field.type === "radio") {
         if (field.checked) payload[field.name] = field.value;
-      } else if (field.type === 'checkbox') {
-        if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
+      } else if (field.type === "checkbox") {
+        if (field.checked)
+          payload[field.name] = payload[field.name]
+            ? `${payload[field.name]},${field.value}`
+            : field.value;
       } else {
         payload[field.name] = field.value;
       }
@@ -44,20 +61,20 @@ function generatePayload(form) {
 }
 
 async function handleSubmit(form) {
-  if (form.getAttribute('data-submitting') === 'true') return;
+  if (form.getAttribute("data-submitting") === "true") return;
 
   const submit = form.querySelector('button[type="submit"]');
   try {
-    form.setAttribute('data-submitting', 'true');
+    form.setAttribute("data-submitting", "true");
     submit.disabled = true;
 
     // create payload
     const payload = generatePayload(form);
     const response = await fetch(form.dataset.action, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({ data: payload }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
     if (response.ok) {
@@ -72,30 +89,32 @@ async function handleSubmit(form) {
     // eslint-disable-next-line no-console
     console.error(e);
   } finally {
-    form.setAttribute('data-submitting', 'false');
+    form.setAttribute("data-submitting", "false");
     submit.disabled = false;
   }
 }
 
 export default async function decorate(block) {
-  const links = [...block.querySelectorAll('a')].map((a) => a.href);
-  const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
+  const links = [...block.querySelectorAll("a")].map((a) => a.href);
+  const formLink = links.find(
+    (link) => link.startsWith(window.location.origin) && link.endsWith(".json")
+  );
   const submitLink = links.find((link) => link !== formLink);
   if (!formLink || !submitLink) return;
 
   const form = await createForm(formLink, submitLink);
   block.replaceChildren(form);
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const valid = form.checkValidity();
     if (valid) {
       handleSubmit(form);
     } else {
-      const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
+      const firstInvalidEl = form.querySelector(":invalid:not(fieldset)");
       if (firstInvalidEl) {
         firstInvalidEl.focus();
-        firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
+        firstInvalidEl.scrollIntoView({ behavior: "smooth" });
       }
     }
   });
